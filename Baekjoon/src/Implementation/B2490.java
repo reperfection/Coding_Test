ee3723177ce1091de5b3f8456af087208672e344
Jung, Jaewan <wodhkszxc@gmail.com>
2022-08-21 02:00:19 +0900

    깃에러

---
+++ Baekjoon/src/Implementation/B2490.java
@@ -0,0 +1,43 @@
+package Implementation;
+// 윷놀이
+
+import java.io.*;
+public class B2490 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(int i=0; i<3; i++) {
+            String play = br.readLine();
+            String [] sp = play.split("\\s");
+            int front = 0;
+            for(int j=0; j<sp.length; j++) {
+                if(sp[j].equals("0"))
+                    front++;
+            }
+            if(front==0) {
+                bw.write("E");
+                bw.newLine();
+            }
+            else if(front==1) {
+                bw.write("A");
+                bw.newLine();
+            }
+            else if(front==2) {
+                bw.write("B");
+                bw.newLine();
+            }
+            else if(front==3) {
+                bw.write("C");
+                bw.newLine();
+            }
+            else {
+                bw.write("D");
+                bw.newLine();
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
--- Baekjoon/src/String/B2743.java
+++ Baekjoon/src/String/B2743.java
@@ -1,10 +1,12 @@
+package String;
+// 단어 길이 재기
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 
-public class Main {
+public class B2743 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
