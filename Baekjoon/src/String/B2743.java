04374dc863471d255c4ab21fde7c9ed849284a42
Jung, Jaewan <wodhkszxc@gmail.com>
2022-08-20 23:45:48 +0900

    단어 길이 재기

---
+++ Baekjoon/src/String/B2743.java
@@ -0,0 +1,17 @@
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String st = br.readLine();
+        int len = st.length();
+        bw.write(Integer.toString(len));
+        bw.flush();
+        bw.close();
+    }
+}
