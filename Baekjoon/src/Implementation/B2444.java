71ccc3d9a3f6022c67d7fb81e81088d918a10d8f
Jung, Jaewan <wodhkszxc@gmail.com>
2022-08-25 22:37:25 +0900

    별찍기 - 7

--- Baekjoon/src/Implementation/B2444.java
+++ Baekjoon/src/Implementation/B2444.java
@@ -1,33 +1,37 @@
-package Implementation;
-// 별찍기 - 7 - 다음번에
-import java.io.BufferedReader;
+// 별 찍기-7
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+
 public class B2444 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int star = Integer.parseInt(br.readLine());
-        br.close();
-
-        for(int i=0; i<star*2-1; i++) {
-            for(int j=i; j<star-1; j++)
-                bw.write(" ");
-            for(int k=0; k<i*2+1; k++)
-                bw.write("*");
-            bw.flush();
-
-            for(int j=1; j<star-1; j++) {
-                bw.write(" ");
+        int num = Integer.parseInt(br.readLine());
+        int num2 = num;
+        for(int i=num-1; i>0; i--) {
+            for(int j=0; j<num; j++) {
+                if(j<i)
+                    bw.write(" ");
+                else
+                    bw.write("*");
             }
-            for(int k=(star-i-2)*2+1; k>0; )
-                bw.write(" ");
-            bw.flush();
+            num++;
+            bw.newLine();
         }
-
-        bw.close();
+        for(int i=0; i<num2; i++) {
+            for(int j=0; j<num; j++) {
+                if(i>j)
+                    bw.write(" ");
+                else
+                    bw.write("*");
+            }
+            bw.newLine();
+            num--;
+        }
+        bw.flush();
     }
 }
