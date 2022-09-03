34868de740cd5c9b7c91bc2034870d3cbbbc48d2
Jung, Jaewan <wodhkszxc@gmail.com>
2022-09-03 19:54:31 +0900

    숫자카드 - 이분탐색

---
+++ Baekjoon/src/자료구조/B10815.java
@@ -0,0 +1,55 @@
+// 숫자 카드 - 이분 탐색
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B10815 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        StringTokenizer st;
+        st = new StringTokenizer(str);
+        int [] arr = new int [n];
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int findnum = Integer.parseInt(br.readLine());
+        String findstr = br.readLine();
+        st = new StringTokenizer(findstr);
+        for(int i=0; i<findnum; i++) {
+            int findnumber = Integer.parseInt(st.nextToken());
+            bw.write(Integer.toString(binarySearch(findnumber, arr)));
+            bw.write(" ");
+        }
+        bw.flush();
+        bw.close();
+    }
+    public static int binarySearch(int key, int arr[]) {
+
+        int mid;
+        int left = 0;
+        int right = arr.length -1;
+
+        while(right >= left) {
+            mid = (right + left) /2;
+
+            if(key ==arr[mid]) {
+                return 1;
+            }
+
+            if (key < arr[mid])
+                right = mid - 1;
+
+            else
+                left = mid +1;
+        }
+        return 0;
+    }
+}
