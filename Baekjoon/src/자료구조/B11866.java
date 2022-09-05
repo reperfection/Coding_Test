e9078715bb4d2234844773ad529df34af56bb4e7
reperfection <wodhkszxc@gmail.com>
2022-09-05 10:51:35 +0900

    요세푸스 문제 0 - 큐

---
+++ Baekjoon/src/자료구조/B11866.java
@@ -0,0 +1,45 @@
+package src.자료구조;
+// 요세푸스 문제0 - 큐
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class B11866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        int howmany = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        Queue<Integer>queue = new LinkedList<>();
+        for(int i=0; i<howmany; i++) {
+            queue.add(i+1);
+        }
+
+        bw.write("<");
+        while(!queue.isEmpty()) {
+            for(int i=0; i<k-1; i++) {
+                int first = queue.peek();
+                queue.remove();
+                queue.add(first);
+            }
+            if(queue.size()!=1) {
+                bw.write(queue.peek()+", ");
+                queue.remove();
+            }
+            else {
+                bw.write(Integer.toString(queue.peek()));
+                queue.remove();
+            }
+        }
+        bw.write(">");
+        bw.flush();
+        bw.close();
+    }
+}
