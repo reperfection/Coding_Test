c6b79da7d35e63719c521c77ac5b3ab34e4608c1
reperfection <wodhkszxc@gmail.com>
2022-09-06 13:10:15 +0900

    단어의 개수

--- Baekjoon/src/String/B1152.java
+++ Baekjoon/src/String/B1152.java
@@ -11,7 +11,7 @@ import java.util.StringTokenizer;
 import java.util.Map;
 import java.util.HashMap;
 
-public class B1152g {
+public class B1152 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
