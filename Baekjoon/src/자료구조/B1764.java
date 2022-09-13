bf07a9614f01f60e0c418003c64c511b0e225324
reperfection <wodhkszxc@gmail.com>
2022-09-13 19:25:26 +0900

    듣보ì - ì해시맵

---
+++ Baekjoon/src/자료구조/B1764.java
@@ -0,0 +1,50 @@
+package src.자료구조;
+// 듣보잡
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class B1764 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String num = br.readLine();
+        StringTokenizer st;
+        st =  new StringTokenizer(num);
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<num1; i++) {
+            String str = br.readLine();
+            map.put(str, i);
+        }
+        int checknum = 0;
+        ArrayList<String> arr = new ArrayList<String>();
+
+        for(int j=0; j<num2; j++) {
+            String check = br.readLine();
+            if(map.containsKey(check)) {
+                checknum++;
+                arr.add(check);
+            }
+        }
+        Collections.sort(arr);
+        bw.write(Integer.toString(checknum));
+        bw.newLine();
+        for(int i=0; i<arr.size(); i++) {
+            bw.write(arr.get(i));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
