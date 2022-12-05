2573a1d20cd6b12284c6746de07ebd12e828254b
reperfection <wodhkszxc@gmail.com>
2022-12-05 15:52:15 +0900

    나는야 포켓몬 마스터 이다솜 - 해시맵(해결)

--- Baekjoon/src/자료구조/B1620.java
+++ Baekjoon/src/자료구조/B1620.java
@@ -1,49 +1,51 @@
 package src.자료구조;
-// 나는야 포켓몬 마스터 이다솜
+// 나는야 포켓몬 마스터 이다솜 - 해시맵
+// 왜 연결리스트로 하고, 해시맵 2개로 했는데도 시간 초과가 떴는지
+// 형변환 (int)를 해주었더니 되었는가? 찾아내기
 import java.io.IOException;
 import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class B1620 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        Map<Integer, String> map = new HashMap<>();
-        String str = br.readLine();
-        st = new StringTokenizer(str);
-        int input = Integer.parseInt(st.nextToken());
+        Map<String, Integer> numMap = new HashMap<>();
+        Map<Integer, String> monsterMap = new HashMap<>();
+        //LinkedList<String> monsters = new LinkedList<>();
+
+        String count = br.readLine();
+        st = new StringTokenizer(count);
+        int cnt = Integer.parseInt(st.nextToken());
         int find = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<input; i++) {
+
+        for(int i=0; i<cnt; i++) {
             String poketmon = br.readLine();
-            map.put(i+1, poketmon);
+            numMap.put(poketmon, i+1);
+            monsterMap.put(i+1, poketmon);
         }
 
+
         for(int i=0; i<find; i++) {
-            String finding = br.readLine();
-            char temp = finding.charAt(0);
-            if(Character.isDigit(temp)==true) {
-                bw.write(map.get(Integer.parseInt(finding)));
+            String str = br.readLine();
+            if((int)str.charAt(0)>47&&(int)str.charAt(0)<58) {
+                bw.write(monsterMap.get(Integer.parseInt(str)));
                 bw.newLine();
             }
             else {
-                for(Integer j : map.keySet()) {
-                    String value = map.get(j);
-                    if(finding.equals(value)) {
-                        bw.write(Integer.toString(j));
-                        bw.newLine();
-                    }
-                }
+                bw.write(Integer.toString(numMap.get(str)));
+                bw.newLine();
             }
         }
         bw.flush();
         bw.close();
     }
 }
-
